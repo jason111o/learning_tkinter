b57fbe6fe1c093d854b5e222776569e34cb2fa32
Jason Pippin <razrjlinux@gmail.com>
2023-02-12 09:43:16 -0600

    Fixed function events

--- Treasure Hunt.py
+++ Treasure Hunt.py
@@ -17,50 +17,53 @@ xr, yr = randint(100, WIDTH - 100), randint(100, HEIGHT - 100)
 
 
 def make_a_mark(event):
-    global counter, xc, yc, xr, yr
-    counter += 1
-    numbers_of_clicks_label.config(text=counter)
-    xc, yc = event.x, event.y
-    if xc in range(xr - 5, xr + 5) and yc in range(yr - 5, yr + 5):
-        how_close_label.config(text='You found the treasure!!!!')
-        next_treasure_button.pack(side=TOP, padx=5, pady=5)
-    elif xc in range(xr - 25, xr + 25) and yc in range(yr - 25, yr + 25):
-        how_close_label.config(text='Hot!!!')
-    elif xc in range(xr - 45, xr + 45) and yc in range(yr - 45, yr + 45):
-        how_close_label.config(text='Warmer!!')
-    elif xc in range(xr - 65, xr + 65) and yc in range(yr - 65, yr + 65):
-        how_close_label.config(text='Warm!')
-    elif xc in range(xr - 85, xr + 85) and yc in range(yr - 85, yr + 85):
-        how_close_label.config(text='Cold!!')
-    elif xc in range(xr - 105, xr + 105) and yc in range(yr - 105, yr + 105):
-        how_close_label.config(text='Colder!!!')
-    else:
-        how_close_label.config(text='Freezing!!!!')
-    if counter % 20 == 0:
-        parrot_button.pack(side=TOP, padx=5, pady=5)
-        x_treasure_coordinates_label.config(text=xr)
-        y_treasure_coordinates_label.config(text=yr)
-    x_coordinates_label.config(text=xc)
-    y_coordinates_label.config(text=yc)
-    canvas.create_oval(event.x - 10, event.y - 10, event.x + 10, event.y + 10, fill='NavajoWhite4')
+    if event:
+        global counter, xc, yc, xr, yr
+        counter += 1
+        numbers_of_clicks_label.config(text=counter)
+        xc, yc = event.x, event.y
+        if xc in range(xr - 5, xr + 5) and yc in range(yr - 5, yr + 5):
+            how_close_label.config(text='You found the treasure!!!!')
+            next_treasure_button.pack(side=TOP, padx=5, pady=5)
+        elif xc in range(xr - 25, xr + 25) and yc in range(yr - 25, yr + 25):
+            how_close_label.config(text='Hot!!!')
+        elif xc in range(xr - 45, xr + 45) and yc in range(yr - 45, yr + 45):
+            how_close_label.config(text='Warmer!!')
+        elif xc in range(xr - 65, xr + 65) and yc in range(yr - 65, yr + 65):
+            how_close_label.config(text='Warm!')
+        elif xc in range(xr - 85, xr + 85) and yc in range(yr - 85, yr + 85):
+            how_close_label.config(text='Cold!!')
+        elif xc in range(xr - 105, xr + 105) and yc in range(yr - 105, yr + 105):
+            how_close_label.config(text='Colder!!!')
+        else:
+            how_close_label.config(text='Freezing!!!!')
+        if counter % 20 == 0:
+            parrot_button.pack(side=TOP, padx=5, pady=5)
+            x_treasure_coordinates_label.config(text=xr)
+            y_treasure_coordinates_label.config(text=yr)
+        x_coordinates_label.config(text=xc)
+        y_coordinates_label.config(text=yc)
+        canvas.create_oval(event.x - 10, event.y - 10, event.x + 10, event.y + 10, fill='NavajoWhite4')
 
 
-def fly_parrot():
-    canvas.create_oval(xr - 80, yr - 80, xr + 80, yr + 80, outline='black')
-    parrot_button.pack_forget()
+def fly_parrot(event):
+    if event:
+        canvas.create_oval(xr - 80, yr - 80, xr + 80, yr + 80, outline='black')
+        parrot_button.pack_forget()
 
 
-def start_again():
-    next_treasure_button.pack_forget()
-    global counter, xc, yc, xr, yr
-    counter = 0
-    xc, yc = 0, 0
-    xr, yr = randint(100, WIDTH - 100), randint(100, HEIGHT - 100)
-    x_coordinates_label.config(text=xc)
-    y_coordinates_label.config(text=yc)
-    x_treasure_coordinates_label.config(text=xc)
-    y_treasure_coordinates_label.config(text=yc)
-    numbers_of_clicks_label.config(text=counter)
+def start_again(event):
+    if event:
+        next_treasure_button.pack_forget()
+        global counter, xc, yc, xr, yr
+        counter = 0
+        xc, yc = 0, 0
+        xr, yr = randint(100, WIDTH - 100), randint(100, HEIGHT - 100)
+        x_coordinates_label.config(text=xc)
+        y_coordinates_label.config(text=yc)
+        x_treasure_coordinates_label.config(text=xc)
+        y_treasure_coordinates_label.config(text=yc)
+        numbers_of_clicks_label.config(text=counter)
 
 
 top_frame = Frame(main)
